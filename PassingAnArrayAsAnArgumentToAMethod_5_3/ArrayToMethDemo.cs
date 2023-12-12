d584cd442a7d2d25f1bf3ab901f53ea6f9da2f6e
Ivan Kudryashov <ivan.kudryash1337@gmail.com>
2023-12-12 14:13:45 +0400

    add listing 5.3 with array like an argument for method

---
+++ PassingAnArrayAsAnArgumentToAMethod_5_3/ArrayToMethDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingAnArrayAsAnArgumentToAMethod_5_3
+{
+    internal class ArrayToMethDemo
+    {
+        // методо для заполнения массива случайными числами:
+        static void fillRand(int[] nums)
+        {
+            // объект для генерирования случайных чисел:
+            Random rnd = new Random();
+            // заполнение массива случайными числами:
+            for (int k = 0; k < nums.Length; k++)
+                nums[k] = rnd.Next(1, 101);
+        }
+
+
+        static void Main(string[] args)
+        {
+            // одномерный массив:
+            int[] B = new int[5];
+
+            // массив заполняется случайными числами:
+            fillRand(B);
+            // Завершение программы
+            Console.Write("Для завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
