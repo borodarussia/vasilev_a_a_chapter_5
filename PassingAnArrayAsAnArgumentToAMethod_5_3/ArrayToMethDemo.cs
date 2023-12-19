fedb7945d6a11e2ca76017b32dde1fd61fbd4014
Ivan Kudryashov <ivan.kudryash1337@gmail.com>
2023-12-19 11:29:06 +0400

    complete listing num 5.3

--- PassingAnArrayAsAnArgumentToAMethod_5_3/ArrayToMethDemo.cs
+++ PassingAnArrayAsAnArgumentToAMethod_5_3/ArrayToMethDemo.cs
@@ -8,7 +8,7 @@ namespace PassingAnArrayAsAnArgumentToAMethod_5_3
 {
     internal class ArrayToMethDemo
     {
-        // методо для заполнения массива случайными числами:
+        // метод для заполнения массива случайными числами:
         static void fillRand(int[] nums)
         {
             // объект для генерирования случайных чисел:
@@ -18,14 +18,74 @@ namespace PassingAnArrayAsAnArgumentToAMethod_5_3
                 nums[k] = rnd.Next(1, 101);
         }
 
-
+        // метод для отображения одномерного целочисленного массива:
+        static void showArray(int[] nums)
+        {
+            // перебор элементов массива:
+            for (int i = 0; i < nums.Length; i++)
+                // отображение значения элемента:
+                Console.Write("| {0}", nums[i]);
+            Console.WriteLine("|");
+        }
+        // метод для отображения двумерного целочисленного массива:
+        static void showArray(int[,] nums)
+        {
+            // перебор строк в массиве:
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int j = 0; j < nums.GetLength(1); j++)
+                    // перебор элементов в строке
+                    Console.Write("{0,3}", nums[i, j]);
+                // переход к новой строке:
+                Console.WriteLine();
+            }
+        }
+        // метод для вычисления наименьшего элемента в массиве:
+        static int findMin(int[] nums)
+        {
+            // локальная переменная
+            int s = nums[0];
+            // поиск наименьшего значения:
+            for (int i = 1; i < nums.Length; i++)
+                // если проверяемый элемент имеет значение,
+                // меньшее текущего значения переменно s:
+                if (nums[i] < s)
+                    s = nums[i];
+            // результат метода
+            return s;
+        }
+        // главный метод программы:
         static void Main(string[] args)
         {
-            // одномерный массив:
+            // одномерные массивы:
+            int[] A = { 1, 3, 5, 7, 9, 11, 13, 15 };
             int[] B = new int[5];
+
+            // двумерный массив:
+            int[,] C = { 
+                { 1, 2, 3, 4}, 
+                { 5, 6, 7, 8}, 
+                { 9, 10, 11, 12} };
 
             // массив заполняется случайными числами:
             fillRand(B);
+
+            Console.WriteLine("Одномерный массив A:");
+            // отображение массива A:
+            showArray(A);
+
+            Console.WriteLine("Одномерный массив B:");
+            // отображение массива B:
+            showArray(B);
+
+            // поиск наименьшего элемента:
+            int m = findMin(B);
+            Console.WriteLine("Наименьшее значение массива B: {0}", m);
+
+            Console.WriteLine("Двумерный массив C:");
+            // отображение массива C:
+            showArray(C);
+
             // Завершение программы
             Console.Write("Для завершения работы программы нажмите Enter...");
             Console.ReadLine();
