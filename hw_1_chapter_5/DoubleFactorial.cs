650d3f978092a89db4ddec8c4979656f99460cfa
Ivan Kudryashov <ivan.kudryash1337@gmail.com>
2023-12-20 16:59:12 +0400

    complete hw num1 and num2

---
+++ hw_1_chapter_5/DoubleFactorial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_1_chapter_5
+{
+    internal class DoubleFactorial
+    {
+        /*
+         * Программа для вычисления двойного факторилаа числа, переданного аргументом методу.
+         * Последний множитель равен 1 для нечетного аргумента и равен 2 для четного аргумента
+         * Предложите версию метода без рекурсии и с рекурсией.
+         */
+
+        // метод с рекурсией
+        static int doubleFactorialRecursion(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Значение аргумента меньше 0");
+            if (n == 0)
+                return 1;
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 2;
+
+            return n * doubleFactorialRecursion(n - 2);
+        }
+
+        // метод без рекурсии
+        static int doubleFactorial(int n)
+        {
+            int res = 1;
+
+            if (n < 0)
+                throw new ArgumentException("Значение аргумента меньше 0");
+            //if (n == 0)
+            //    res = 1;
+
+            while (n > 1)
+            {
+                res *= n;
+                n -= 2;
+            }
+
+            return res;
+        }
+
+
+        static void Main(string[] args)
+        {
+            // двойной факториал числа
+            Console.WriteLine("Двойной факториал числа (рекурсия):");
+            for (int i = 0; i <= 13; i++)
+            {
+                // вычисление двойного факториала числа:
+                Console.WriteLine(i.ToString() + "!! = "
+                    + doubleFactorialRecursion(i).ToString() + " - "
+                    + doubleFactorial(i).ToString());
+            }
+
+            // doubleFactorialRecursion(-2);
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hw_2_chapter_5/SumSquareNaturalValue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_2_chapter_5
+{
+    internal class SumSquareNaturalValue
+    {
+        /*
+         * Программа для вычисления суммы квадратов натуральных чисел
+         * Аргумент метода - натуральное число
+         * Предложите версию метода без рекурсии и с рекурсией
+         */
+
+        // метод с рекурсией
+        static int squareSumRecursion(int n)
+        {
+            if (n == 1)
+                return 1;
+
+            return n * n + squareSumRecursion(n -1);
+        }
+        // метод без рекурсии
+        static int squareSum(int n)
+        {
+            int res = 0;
+
+            for (int i = n; i > 0; i--)
+                res += i * i;
+
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            // сумма квадратов натуральных чисел
+            Console.WriteLine("Сумма квадратов натуральных чисел: ");
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(i.ToString() + " = "
+                    + squareSumRecursion(i).ToString() + " - "
+                    + squareSum(i));
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
