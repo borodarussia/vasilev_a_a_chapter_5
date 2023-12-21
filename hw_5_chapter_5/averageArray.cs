7bfa6641c8662f400310782d77896c2cc1eb3b98
Ivan Kudryashov <ivan.kudryash1337@gmail.com>
2023-12-21 10:40:35 +0400

    complete hw num 5 from chapter 5

---
+++ hw_5_chapter_5/averageArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_5_chapter_5
+{
+    internal class averageArray
+    {
+        /*
+         * Программа со статическим методом, аругментом которому передается
+         * целочисленный массив, а результатом возвращается среднее значение для элементов массива
+         * (сумма значений элементов, деленная на количество элементов в массиве)
+         */
+
+        static double averageOfArr(int[] arr)
+        {
+            double averageValue = 0.0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {                
+                averageValue += (double)arr[i] / (double)arr.Length;
+            }
+
+            return averageValue;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] checkArr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            double averageOfCheckArr = averageOfArr(checkArr);
+
+            Console.WriteLine("Среднее значение массива checkArr:\t{0}", averageOfCheckArr);
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
