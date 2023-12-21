4639672b3dd60658a5508ba04d24e7e55caa74c0
Ivan Kudryashov <ivan.kudryash1337@gmail.com>
2023-12-21 13:38:05 +0400

    complete hw num 9 and num 10 from chapter 5

---
+++ hw_10_chapter_5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_10_chapter_5
+{
+    internal class Program
+    {
+        /*
+         * Программма со статическим методом,
+         * которому передается текст и произвольное количество символьных аргументов.
+         * Результатом возвращается текст,
+         * который получается добавлением в конец исходного текста
+         * (первый аргумент метода) символьных значений,
+         * переданных аргументами методу.
+         */
+        static string modifStr(string str, params char[] symbs)
+        {
+            string mdfStr = str + " - ";
+
+            for (int i = 0; i < symbs.Length; i++)
+                mdfStr += symbs[i].ToString();
+
+            return mdfStr;
+        }
+        static void Main(string[] args)
+        {
+            string firstStr = modifStr("Проверочный текст 1-го варианта", 'б', 'с');
+            Console.Write(firstStr);
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hw_9_chapter_5/MinMaxOfElements.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_9_chapter_5
+{
+    internal class MinMaxOfElements
+    {
+        /*
+         * Программа со статическим методом, аргументом которому
+         * передается произвольное количество целочисленных аргументов.
+         * Результатом метод возвращает массив из двух элементов:
+         * это значения наибольшего и анименьшего значений среди аргументов,
+         * переданных методу
+         */
+
+        static int[] minMaxValues(params int[] values)
+        {
+            if (values.Length == 0)
+                throw new ArgumentException();
+
+            int minValue = values[0];
+            int maxValue = values[0];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if(minValue > values[i])
+                    minValue = values[i];
+                if(maxValue < values[i])
+                    maxValue = values[i];
+            }
+
+            int[] resArr = { minValue, maxValue };
+            return resArr;
+        }
+
+        static void showArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == 0)
+                    Console.Write("[ {0} | ", arr[i]);
+                else if (i == arr.Length - 1)
+                    Console.WriteLine("{0} ]", arr[i]);
+                else
+                    Console.Write("{0} | ", arr[i]);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] minMaxFirstTry = minMaxValues(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            int[] minMaxSecondTry = minMaxValues(5);
+            int[] minMaxThirdTry = minMaxValues(6, 5, 123, 6, 89734, 54682, 7484);
+
+
+            showArray(minMaxFirstTry);
+            showArray(minMaxSecondTry);
+            showArray(minMaxThirdTry);
+
+
+            // Завершение программы
+            Console.Write("\n\n\nДля завершения работы программы нажмите Enter...");
+            Console.ReadLine();
+        }
+    }
+}
